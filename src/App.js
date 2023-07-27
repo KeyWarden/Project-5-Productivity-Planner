d93c88909622aa2a196e33a8812b08f99151b8bb
KeyWarden <ekeeley9@gmail.com>
2023-07-27 13:41:14 +0000

    link bootstrap stylesheet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Test
-        </p>
-      </header>
+      <Button variant="primary">primary</Button>
     </div>
   );
 }
