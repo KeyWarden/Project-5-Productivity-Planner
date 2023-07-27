559a3f362e1d80e901f089d9363d8eae4a0a10e0
KeyWarden <ekeeley9@gmail.com>
2023-07-27 15:04:42 +0000

    install django

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header className='App-header'>
+        Navigation between Tasks and Projects
+      </header>
       <Button variant="primary">primary</Button>
+      <footer className='App-footer'>
+        Contact Details
+      </footer>
     </div>
   );
 }
