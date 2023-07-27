86a435a469df2ff698f93614cbbabf1dbc105b5c
KeyWarden <ekeeley9@gmail.com>
2023-07-27 15:31:52 +0000

    add cloudinary to installed apps

--- pro_plan/settings.py
+++ pro_plan/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
