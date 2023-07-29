9eadbd22d6ef9ab5b142a01a40e836a17902fcad
KeyWarden <ekeeley9@gmail.com>
2023-07-29 06:07:33 +0000

    add read-only permissions

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from pro_plan.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(APIView):
@@ -17,9 +18,11 @@ class ProfileList(APIView):
 class ProfileDetail(APIView):
     """Manages Profile Data Retrieval and Submission"""
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
