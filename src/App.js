53cc327801471f5b8547deca30af631e236c429a
KeyWarden <ekeeley9@gmail.com>
2023-07-29 09:02:32 +0000

    add basic styling + filler content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
-        Navigation between Tasks and Projects
+        <p>Pro<span className='Title2'>Plan</span><span className='Title3'>X</span></p>
       </header>
-      <Button variant="primary">primary</Button>
+      <div className='App-content'>
+        <p>Filler</p>
+      </div>
       <footer className='App-footer'>
-        Contact Details
+        <h3>Contact Details</h3>
+        <p>Phone Number: 01234 56789</p>
+        <p>Email: admin@proplanx.corp</p>
       </footer>
     </div>
   );
