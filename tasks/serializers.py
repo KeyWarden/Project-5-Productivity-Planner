ee7963423f68d9a131fd4f14984c0458d6142af4
KeyWarden <ekeeley9@gmail.com>
2023-07-29 12:27:50 +0000

    add Group urls

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -17,3 +17,19 @@ class TaskSerializer(serializers.ModelSerializer):
             'id', 'owner', 'created_at', 'updated_at', 'due_at',
             'title', 'description', 'is_owner'
         ]
+
+class GroupSerializer(serializers.ModelSerializer):
+    """serializes the Group data"""
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = TaskGroup
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at',
+            'task', 'description', 'group_size', 'is_owner'
+        ]
--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('tasks/', views.TaskList.as_view()),
     path('api-auth/', include('rest_framework.urls')),
     path('tasks/<int:pk>/', views.TaskDetail.as_view()),
+    path('groups/', views.GroupListList.as_view()),
+    path('groups/<int:pk>/', views.GroupDetail.as_view()),
 ]
